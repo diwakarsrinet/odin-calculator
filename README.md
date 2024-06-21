681e9371a8d6a8c2b07bd7d74269ce2abee3b256
diwakarsrinet <169426962+diwakarsrinet@users.noreply.github.com>
2024-06-21 12:26:55 +0530

    All the functionalities and features with Javascript added. Calculator ready for use and test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-calculator
+
+Image Courtesy: background.jpg https://www.pexels.com/@gdtography-277628/
--- caljscript.js
+++ caljscript.js
@@ -1,38 +1,103 @@
-let firstValue, secondValue = 0;
-let operator = "";
+// Variables for storage
+let firstValue = "", secondValue = "", result = 0;
+let firstOperator = "", secondOperator = "";
+let equalPressed = 0;
+let decimalValues = "0123456789";
+let operatorValues = "+-/*";
 
-function getSum (a,b) {
-    return a+b;
+// Function to clear current display
+function clearAll() {
+    const inputElement = document.querySelector("#displayInput");
+    const resultElement = document.querySelector("#displayResult");
+    inputElement.value = "", resultElement.value = "";
+    firstValue = "", secondValue = "", result = 0;
+    firstOperator = "", secondOperator = "";
 }
 
-function getDifference (a,b) {
-    return a-b;
+// Pick operands and operators
+const btnPressed = document.querySelectorAll(".btnCal");
+btnPressed.forEach(btn => {
+    btn.addEventListener("click", () => checkButtonType(btn.innerHTML));
+});
+
+// Function to check button type and call relevant functions
+function checkButtonType(btnValue) {
+    if (decimalValues.includes(btnValue)) btnNumPressed(btnValue);
+    else if (operatorValues.includes(btnValue)) btnOpPressed(btnValue);
+    else if (btnValue===".") dotPressed();
+    else if (btnValue === "AC") clearAll();
+    else if (btnValue === "=") {
+        showResult();
+        equalPressed = 1;
+    }
 }
 
-function getProduct (a,b) {
-    return a*b;
+// Input numbers on numeral button clicks
+function btnNumPressed(btn) {
+    const inputElement = document.querySelector("#displayInput");
+    if(equalPressed === 1) {
+        firstValue = "";
+        firstValue += btn;
+        equalPressed = 0;
+    }
+    else if(firstOperator === ""){
+        firstValue += btn;
+    }
+    else {
+        secondValue += btn;
+    }
+    inputElement.value = firstValue + firstOperator + secondValue;
 }
 
-function getDivision (a,b) {
-    return a/b;
+// Operator button pressed
+function btnOpPressed(btn) {
+    const inputElement = document.querySelector("#displayInput");
+    if(firstOperator === "") {
+        firstOperator = btn;
+        inputElement.value = firstValue + firstOperator + secondValue;
+    }
+    
+    else if (secondOperator === "") {
+        secondOperator = btn;
+        showResult();
+        inputElement.value = firstValue + firstOperator + secondValue;
+    }
 }
+
+// Decimal point (dot) pressed
+function dotPressed() {
+    const inputElement = document.querySelector("#displayInput");
+    if (!(firstValue.includes(".")) && firstOperator === "") firstValue += ".";
+    else if (!(secondValue.includes(".")) && firstOperator !== "") secondValue += ".";
+    inputElement.value = firstValue + firstOperator + secondValue;
+}
+
+// Show result by calling Operate function when = or second operator button is pressed and pass on two numbers & the operator
+function showResult() {
+    const outputElement = document.querySelector("#displayResult");
+    result = operate(firstValue,firstOperator,secondValue);
+    result = Math.round(result*100)/100;
+    outputElement.value = result; 
+    firstValue = result.toString();
+    firstOperator = secondOperator;
+    secondOperator = "", secondValue = "", result = "";
+}
+
 
 function operate (x,y,z) {
-    firstValue = x;
-    operator = y;
-    secondValue = z;
-    switch(operator) {
+    switch(y) {
         case "+": 
-            getSum(firstValue,secondValue); 
+            return (parseFloat(x) + parseFloat(z))
             break;
         case "-": 
-            getDifference(firstValue,secondValue);
-            break;
+            return (parseFloat(x) - parseFloat(z));
+        break;
         case "*": 
-            getProduct(firstValue,secondValue);
-            break;
+            return (parseFloat(x) * parseFloat(z));
+        break;
         case "/": 
-            getDivision(firstValue,secondValue);     
+            return (parseFloat(x) / parseFloat(z));
     }
 
 }
+
